954a9bebe5f1302638766637ed11a7a67dd20029
diarmaidm <diarmaidm@users.noreply.github.com>
2017-06-08 16:03:20 +0100

    fix expect statement

--- test/factors_test.js
+++ test/factors_test.js
@@ -1,8 +1,9 @@
+// const expect = require('chai');
 const expect = require('chai').expect;
 const factors = require('./factors');
 
 describe('Prime Factors calculator', function() {
     it('returns the prime factors of a given integer', function() {
-        expect(factors(1).to.eql([]));
+        expect(factors(1)).to.eql([]);
     });
 });

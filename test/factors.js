f1314c517c927b99dda89f5fa345abefc73df96f
diarmaidm <diarmaidm@users.noreply.github.com>
2017-06-08 18:08:04 +0100

    pass 4th test - factors of 4 – it fails the 2nd test factors of 2

--- test/factors.js
+++ test/factors.js
@@ -1,6 +1,10 @@
 module.exports = factorsOf = (n) => {
     let factors = [];
     if (n > 1) {
+        if (n % 2 === 0) {
+            factors.push(2);
+            n /= 2;
+        }
         factors.push(n);
     }
     return factors;

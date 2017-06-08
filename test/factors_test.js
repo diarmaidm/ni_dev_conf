9f1e36b36ce3546d2a91536c39bf6edda359f610
diarmaidm <diarmaidm@users.noreply.github.com>
2017-06-08 17:24:58 +0100

    add test for factors of 2

--- test/factors_test.js
+++ test/factors_test.js
@@ -4,5 +4,7 @@ const factors = require('./factors');
 describe('Prime Factors calculator', function() {
     it('returns the prime factors of a given integer', function() {
         expect(factors(1)).to.eql([]);
+
+        expect(factors(2)).to.eql([2]);
     });
 });

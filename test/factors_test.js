785f7fea4178cff81cb25c1fc228e0cdcf15a984
diarmaidm <diarmaidm@users.noreply.github.com>
2017-06-08 15:00:28 +0100

    Add failing test

--- test/factors_test.js
+++ test/factors_test.js
@@ -2,6 +2,6 @@ const expect = require('chai').expect;
 
 describe('Prime Factors calculator', function() {
     it('returns the prime factors of a given integer', function() {
-
+        expect(factors(1).to.eql([]));
     });
 });

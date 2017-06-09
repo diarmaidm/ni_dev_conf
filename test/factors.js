dcb1ec5a72a4f82ae97fb9f047db0fbc4a57e6a0
diarmaidm <diarmaidm@users.noreply.github.com>
2017-06-09 01:59:38 +0100

    cleanup after refactor

--- test/factors.js
+++ test/factors.js
@@ -7,23 +7,7 @@ module.exports = factorsOf = (n) => {
             n /= divisor;
         }
         divisor++;
-        // while (n % 3 === 0) {
-        //     factors.push(3);
-        //     n /= 3;
-        // }
-        // if (n % 5 === 0) {
-        //     factors.push(5);
-        //     n /= 5;
-        // }
-        // if (n % 7 === 0) {
-        //     factors.push(7);
-        //     n /= 7;
-        // }
     }
-
-    // if (n > 1) {
-    //     factors.push(n);
-    // }
 
     return factors;
 };

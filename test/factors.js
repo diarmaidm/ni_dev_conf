e84b26b5eb3129e909130a241c6c0cb0622bab0f
diarmaidm <diarmaidm@users.noreply.github.com>
2017-06-09 01:59:10 +0100

    refactor implementation code

--- test/factors.js
+++ test/factors.js
@@ -1,28 +1,29 @@
 module.exports = factorsOf = (n) => {
     let factors = [];
     let divisor = 2;
-    if (n > 1) {
-        while (n % 2 === 0) {
-            factors.push(2);
-            n /= 2;
+    while (n > 1) {
+        while (n % divisor === 0) {
+            factors.push(divisor);
+            n /= divisor;
         }
-        while (n % 3 === 0) {
-            factors.push(3);
-            n /= 3;
-        }
-        if (n % 5 === 0) {
-            factors.push(5);
-            n /= 5;
-        }
-        if (n % 7 === 0) {
-            factors.push(7);
-            n /= 7;
-        }
+        divisor++;
+        // while (n % 3 === 0) {
+        //     factors.push(3);
+        //     n /= 3;
+        // }
+        // if (n % 5 === 0) {
+        //     factors.push(5);
+        //     n /= 5;
+        // }
+        // if (n % 7 === 0) {
+        //     factors.push(7);
+        //     n /= 7;
+        // }
     }
 
-    if (n > 1) {
-        factors.push(n);
-    }
+    // if (n > 1) {
+    //     factors.push(n);
+    // }
 
     return factors;
 };
